369e99a58140cc210a48348d1d78a5bc9052de52
Kim Zuniga <kim.eli.zuniga@gmail.com>
2020-05-10 11:48:19 -0400

    edit navbar, add new.ejs to create new projects

--- app.js
+++ app.js
@@ -11,6 +11,21 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 
+const projectSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    description: String
+});
+
+const Project = mongoose.model("Project", projectSchema);
+// Project.create(
+//     {
+//     title: "Project Title",
+//     image: "https://images.unsplash.com/photo-1559589689-577aabd1db4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+//     description: "Filler description"
+//     }, (err, project) => {
+//         console.log(project);
+// })
 //=============
 // ROUTES
 //=============
@@ -42,7 +57,36 @@ app.get("/contact", (req, res) => {
 // PROJECTS ROUTE
 
 app.get("/projects", (req, res) => {
-    res.render("projects")
+    Project.find({}, (err, allProjects) => {
+        if(err){
+            console.log(err);
+        } else {
+            res.render("projects", {projects: allProjects});
+        }
+    })
+});
+
+// NEW ROUTE
+
+app.get("/projects/new", (req, res) => {
+    res.render("new");
+});
+
+// CREATE ROUTE
+
+app.post("/projects", (req, res) => {
+    const title = req.body.title,
+        image = req.body.image,
+        description = req.body.description,
+        newProject = {title: title, image: image, description: description};
+    
+    Project.create(newProject, (err, newCreated) => {
+        if(err){
+            console.log(err);
+        } else {
+            res.redirect("projects");
+        }
+    })
 });
 
 // SHOW ROUTE
